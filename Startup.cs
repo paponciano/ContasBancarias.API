6581281a4aeb5b6e29bbd9d9562dec653305c7ed
Paulo Augusto Ponciano <paponciano@gmail.com>
2021-10-28 15:03:17 -0300

    Habilitando CORS

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace ContasBancarias.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -43,6 +43,8 @@ namespace ContasBancarias.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ContasBancarias.API v1"));
             }
+
+            app.UseCors(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
